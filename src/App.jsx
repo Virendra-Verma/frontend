8db50691ee192c740ca8381dfdb960bf3f4fce5f
Virendra-Verma <vkverma991099@gmail.com>
2023-07-16 18:22:58 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,24 @@ import './App.css';
 import Home from './Components/Home';
 import Login from './Components/Login';
 import Signup from './Components/Signup';
+import { BrowserRouter, Link, Navigate, Route, Routes } from 'react-router-dom';
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
-    <div>   
-      <Home></Home>
-      <Login/>
-      <Signup/>
+    <div> 
+       <BrowserRouter>
+       <Navbar/>
+      
+     
+        <Routes>
+          <Route path='/' element={<Navigate to ='/Home'/>}/>
+          <Route path='Home' element={<Home/>}/>
+          <Route path='Login' element={<Login/>}/>
+          <Route path='Signup' element={<Signup/>}/>
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -4,6 +4,11 @@ const Home = () => {
   return (
     <div>
         <h1>HomePage</h1>
+        <p style={{fontSize:'50px', backgroundColor:'lightblue'}}>Inline CSS</p>
+        <p className='myclass'>we use className here in JSX</p>
+        <input type='text'/>
+        <br />
+        <br/>
     </div>
   )
 }
