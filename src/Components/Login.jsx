5049c9e73c4cb0a66f4a68fd97a69088b12d144b
Virendra-Verma <vkverma991099@gmail.com>
2023-07-22 14:00:30 +0530

    Login&signup form

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,20 +1,48 @@
+import { useFormik } from 'formik'
 import React from 'react'
+import * as Yup from 'yup';
+
+const LoginSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Required'),
+});
 
 const Login = () => {
-  return (
-    <form className='login'>
-      <div>
-        <label htmlFor="email">Email</label>
-        <input type="text" name='email' id='email'/>
-      </div>
-      <div>
-        <label htmlFor="Pasword" >Password</label>
-        <input type="text" name='Password' id='Password'/>
-      </div>
-      <button type='sumbit'>Login</button>
+  
+  // initializing formik
+  const LoginForm = useFormik({
+    initialValues: {
+      email: ' ',
+      password: ' '
+    },
 
-    </form>
-  )
-}
+    onSubmit: (values) => {
+      console.log(values);
+      // submit values form 
+    },
+    validationSchema:LoginSchema
+  });
+
+  return (
+
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="card w-25 shadow-lg rounded-5">
+        <div className="card-body p-5">
+          <i className="fa-solid fa-lock fa-3x d-block text-center" />
+          <h2 className="text-center my-5">Login Form</h2>
+          <form onSubmit={LoginForm.handleSubmit}>
+            <label htmlFor="">Email</label>
+            <p>{LoginForm.errors.email}</p>
+            <input className="form-control mb-4 rounded-5" type="email" name='email' onChange={LoginForm.handleChange} value={LoginForm.values.email}/>
+            <label htmlFor="">Password</label>
+            <input className="form-control mb-4 rounded-5" type="password" name='password' onChange={LoginForm.handleChange} value={LoginForm.values.password}/>
+            <button type='submit'className="btn btn-danger w-100 mt-4 rounded-5">
+              Login
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+      )
+    }
 
 export default Login
--- src/Components/Signup.jsx
+++ src/Components/Signup.jsx
@@ -1,19 +1,40 @@
+import { useFormik } from 'formik';
 import React from 'react'
 
 const Signup = () => {
-  return (
-    <form className='login'>
-    <div className='contain'>
-      <label htmlFor="email">Email </label>
-      <input type="text" name='email' id='email'/>
-    </div>
-    <div>
-      <label htmlFor="Pasword" > Password</label>
-      <input type="text" name='Password' id='Password'/>
-    </div>
-    <button type='sumbit'>Signup</button>
+   // initializing formik
+   const SignupForm = useFormik({
+    initialValues: {
+      name:' ',
+      email: ' ',
+      password: ' '
+    },
 
-  </form>
+    onSubmit: (values) => {
+      console.log(values);
+      // submit values form 
+    }
+  });
+  return (
+    <div className="d-flex justify-content-center align-items-center vh-100">
+      <div className="card w-25 shadow-lg rounded-5">
+        <div className="card-body p-5">
+          <i className="fa-solid fa-lock fa-3x d-block text-center" />
+          <h2 className="text-center my-5">Singup Form</h2>
+          <form onSubmit={SignupForm.handleSubmit}>
+            <label htmlFor="">Name</label>
+            <input className="form-control mb-4 rounded-5" type="text" name='name' onChange={SignupForm.handleChange} value={SignupForm.values.name} />
+            <label htmlFor="">Email</label>
+            <input className="form-control mb-4 rounded-5" type="email"  name='email' onChange={SignupForm.handleChange} value={SignupForm.values.email} />
+            <label htmlFor="">Password</label>
+            <input className="form-control mb-4 rounded-5" type="password"  name='password' onChange={SignupForm.handleChange} value={SignupForm.values.password} />
+            <button type ="submit" className="btn btn-danger w-100 mt-4 rounded-5">
+              Signup
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   )
 }
 
